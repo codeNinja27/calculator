be0af1b2fb0a358ed765adf5cbed6b9f456afdf2
Tharaka Wijekularatne <tharusnow@gmail.com>
2024-05-20 19:14:22 -0700

    added Modulus operator and static converted double to int

--- main.cpp
+++ main.cpp
@@ -1,24 +1,27 @@
-//This is a calculator app 
+//This is a calculator app which have operations +, -, *, /, %
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
 int main() {
     char opr;
-    int num1, num2;
-    int result;
+    double num1, num2;
+    double result;
 
     cout << "enter an operator" << endl;
     cin >> opr;
 
-    while(opr != '+' && opr != '-' && opr != '*' && opr != '/') {
-        cout << "Please enter a valid operator +, -, *, /" << endl;
+    //Will keep asking the user to enter a valud operator untill they give one of the expected ones. 
+    while(opr != '+' && opr != '-' && opr != '*' && opr != '/' && opr != '%') {
+        cout << "Please enter a valid operator +, -, *, /, %" << endl;
         cin >> opr;
     }
 
     cout << "enter two numbers" << endl;
     cin >> num1;
     cin >> num2;
-
+ 
     switch(opr) {
         case '+':
             result = num1 + num2;
@@ -31,6 +34,9 @@ int main() {
             break;
         case '/':
             result = num1 / num2;
+            break;
+        case '%':
+            result = static_cast<int>(num1) % static_cast<int>(num2);  //static convertion from double to int because % only takes int
             break;
         default:
             cout << "Invalid operator" << endl;
